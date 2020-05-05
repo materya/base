159a8446320371ea2c3804f669d79b19cd014b61
Sylver <sylver@materya.io>
2020-05-05 14:34:27 +0200

    refactor: improve code readability

--- test/promise/sequential.test.ts
+++ test/promise/sequential.test.ts
@@ -19,13 +19,15 @@ describe('sequential', () => {
   it('should resolve sequentially', async () => {
     const sequence = [1, 2, 3, 4, 5]
     const results = await sequential(sequence, promise)
+
     expect(resolveOrder).to.deep.equal(sequence)
     expect(results).to.deep.equal(sequence)
   })
 
-  it('Promise.all() should not be sequential', async () => {
+  it('should not be sequential with Promise.all()', async () => {
     const sequence = [1, 2, 3, 4, 5]
     const results = await Promise.all(sequence.map(promise))
+
     expect(resolveOrder).to.deep.equal(sequence.slice().reverse())
     expect(results).to.deep.equal(sequence)
   })
