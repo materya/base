22546352dca21dfc1bc5db0de9f393adaf22f696
Sylver <sylver@materya.io>
2020-05-20 01:20:21 +0200

    feat(fs/crawl): handle nicely missing directory

--- src/fs/crawl.ts
+++ src/fs/crawl.ts
@@ -1,29 +1,39 @@
 import * as fs from 'fs'
 
+import { FileNotFoundError } from './errors'
+
 /**
  * Callback for adding two numbers.
  *
- * @callback fileActionCallback
+ * @callback FileActionCallback
  *
  * @param {string} name - Filename.
  * @param {string} path - Full file local path.
  */
-export type fileActionCallback = (name: string, path: string) => void
+export type FileActionCallback = (name: string, path: string) => void
 
 /**
  * Crawl recursively in a given directory, triggering a callback for each file.
  *
+ * @module crawl
+ *
  * @param {string} directory - The directory to start in.
- * @param {fileActionCallback} action - Action to trigger for each file found.
+ * @param {FileActionCallback} action - Action to trigger for each file found.
  * @param {string | null} truncate - Part of the directory path to remove
  *  in the final filename.
  */
 const triggerOnFile = (
   directory: string,
-  action: fileActionCallback,
+  action: FileActionCallback,
   truncate: string | null = null,
 ): void => {
   const prefix = truncate || directory
+  try {
+    fs.statSync(directory).isDirectory()
+  } catch (error) {
+    throw new FileNotFoundError(directory)
+  }
+
   fs.readdirSync(directory).forEach(item => {
     const path = `${directory}/${item}`
     if (fs.lstatSync(path).isDirectory()) {
--- test/fs/crawl.test.ts
+++ test/fs/crawl.test.ts
@@ -1,8 +1,9 @@
 import * as mock from 'mock-fs'
 import { expect } from 'chai'
 
-import type { fileActionCallback } from '../../src/fs/crawl'
+import type { FileActionCallback } from '../../src/fs'
 import { crawl } from '../../src/fs'
+import { FileNotFoundError } from '../../src/fs/errors'
 
 const basePath = 'relative/path'
 
@@ -30,11 +31,17 @@ describe('crawl', () => {
   })
 
   it('should pass the right values to callback function', () => {
-    const cb: fileActionCallback = (name, path) => {
+    const cb: FileActionCallback = (name, path) => {
       expect(results).to.includes(name)
       expect(path).to.equals(`${basePath}/${name}`)
     }
 
     crawl.triggerOnFile(basePath, cb)
+  })
+
+  it('should raise if directory does not exist', () => {
+    const cb: FileActionCallback = (_name, _path): void => { /* do nothing */ }
+    expect(() => crawl.triggerOnFile('foo/bar', cb))
+      .to.throw(FileNotFoundError)
   })
 })
