ea27986d2b2a982a6349d8a0eb8ec9967a7bdf37
Sylver <sylver@materya.io>
2020-05-20 01:17:57 +0200

    test: improve setup & lint

--- .mocharc.js
+++ .mocharc.js
@@ -2,6 +2,7 @@ module.exports = {
   require: [
     'ts-node/register',
     'tsconfig-paths/register',
+    './test/mocha.env.ts',
   ],
   timeout: 10000,
   recursive: true,
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,5 +1,16 @@
 module.exports = {
   env: {
-    mocha: true,
+      mocha: true,
+  },
+  plugins: ['mocha'],
+  'extends': [
+    'plugin:mocha/recommended',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': ['error', {
+      'devDependencies': ['**/*.test.ts', 'test/**/*'],
+    }],
+    'mocha/no-mocha-arrows': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
   },
 }
