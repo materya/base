60ea748d96c584fcd2d0f3c6ae2425aad0e59160
Sylver <sylver@materya.io>
2020-05-20 10:14:10 +0200

    refactor: crawl module arrangement

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -1,4 +1,3 @@
 export { default as crawl } from './crawl'
 export { default as find } from './find'
 export * from './crawl'
-export * from './list'
--- test/fs/crawl.test.ts
+++ test/fs/crawl.test.ts
@@ -45,3 +45,99 @@ describe('crawl', () => {
       .to.throw(FileNotFoundError)
   })
 })
+
+describe('list', () => {
+  beforeEach(() => {
+    mock({
+      [basePath]: {
+        file1: 'content',
+        file2: 'content',
+        directory1: {
+          file3: 'content',
+          file4: 'content',
+          directory2: {
+            file5: 'content',
+          },
+          directory3: {
+            file6: 'content',
+            directory4: {},
+          },
+        },
+      },
+    })
+  })
+
+  afterEach(() => {
+    mock.restore()
+  })
+
+  it('should list current files without depth', () => {
+    expect(crawl.list(basePath)).to.deep.equal({
+      files: ['file1', 'file2'],
+      directories: {},
+    })
+  })
+
+  it('should follow directories with one depth', () => {
+    expect(crawl.list(basePath, 1)).to.deep.equal({
+      files: ['file1', 'file2'],
+      directories: {
+        directory1: {
+          directories: {},
+          files: ['file3', 'file4'],
+        },
+      },
+    })
+  })
+
+  it('should follow directories with more depths', () => {
+    expect(crawl.list(basePath, 2)).to.deep.equal({
+      files: ['file1', 'file2'],
+      directories: {
+        directory1: {
+          files: ['file3', 'file4'],
+          directories: {
+            directory2: {
+              directories: {},
+              files: ['file5'],
+            },
+            directory3: {
+              directories: {},
+              files: ['file6'],
+            },
+          },
+        },
+      },
+    })
+  })
+
+  it('should handle empty directories', () => {
+    expect(crawl.list(basePath, 5)).to.deep.equal({
+      files: ['file1', 'file2'],
+      directories: {
+        directory1: {
+          files: ['file3', 'file4'],
+          directories: {
+            directory2: {
+              directories: {},
+              files: ['file5'],
+            },
+            directory3: {
+              files: ['file6'],
+              directories: {
+                directory4: {
+                  files: [],
+                  directories: {},
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+  })
+
+  it('should raise if directory does not exist', () => {
+    expect(() => crawl.list('foo/bar', 1)).to.throw(FileNotFoundError)
+  })
+})
